b63b62797301fbcb12a5e58751096a8fb54ac479
PHSTD <c.oadib.autoan123@gmail.com>
2025-07-25 15:21:07 +0900

    [Feat] getter,setter 추가 - 공격속도 관련 넘어감 작업 필요
    공격속도 관련 미완료

--- Assets/JGH/Scripts/WeaponManager.cs
+++ Assets/JGH/Scripts/WeaponManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class ManagerWeapon : MonoBehaviour
+public class WeaponManager : MonoBehaviour
 {
-    public static ManagerWeapon Instance { get; private set; }
+    public static WeaponManager Instance { get; private set; }
+    public BarrelWeapon barrelWeapon;
     
     // 현재 장착된 무기
-    public BaseWeapon currentWeapon;
+    public BaseWeapon baseWeapon;
     
     private void Awake()
     {
@@ -20,4 +22,145 @@ public class ManagerWeapon : MonoBehaviour
         DontDestroyOnLoad(gameObject); // 씬 전환 시에도 유지
     }
 
+    /// <summary>
+    /// 큰 탄알 조작 함수
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetIsBulletBig(bool value)
+    {
+        baseWeapon.isBigBullet = value;
+    }
+    
+    /// <summary>
+    /// 큰 탄알 여부 함수
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public bool GetIsBulletBig(bool value)
+    {
+        return baseWeapon.isBigBullet;
+    }
+    
+    
+    /// <summary>
+    /// 폭발성 탄알 조작 함수
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetIsBulletExplosive(bool value)
+    {
+        baseWeapon.isExplosiveBullet = value;
+    }
+    
+    /// <summary>
+    /// 폭발성 탄알 여부 함수
+    /// </summary>
+    /// <param name="value"></param>
+    public bool GetIsBulletExplosive()
+    {
+        return baseWeapon.isExplosiveBullet;
+    }
+
+    /// <summary>
+    /// 탄창 조작 함수
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetBullet(int value)
+    {
+        baseWeapon.maxAmmo = value;
+        baseWeapon.currentAmmo = value;
+    }
+    
+    /// <summary>
+    /// 현재 탄창 갯수 반환 함수
+    /// </summary>
+    /// <param name="value"></param>
+    public void GetCurrectBullet(int value)
+    {
+        baseWeapon.currentAmmo = value;
+    }
+    
+    /// <summary>
+    /// 최대 탄창 갯수 반환 함수
+    /// </summary>
+    /// <param name="value"></param>
+    public void GetMaxBullet(int value)
+    {
+        baseWeapon.maxAmmo = value;
+    }
+    
+    /// <summary>
+    /// 탄창 조작 함수(샷건)
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetBarrelWeapon(int value)
+    {
+        barrelWeapon.ammoPerShot = value;
+    }
+    
+    /// <summary>
+    /// 샷건 탄창 갯수 반환 함수
+    /// </summary>
+    /// <returns></returns>
+    public int GetBarrelWeapon()
+    {
+        return barrelWeapon.ammoPerShot;
+    }
+    
+    /// <summary>
+    /// 데미지 조작 함수
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetAttackDamage(int value)
+    {
+        baseWeapon.attackDamage = value;
+    }
+    
+    /// <summary>
+    /// 데미지 값 반환 함수
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public int GetAttackDamage()
+    {
+        return baseWeapon.attackDamage;
+    }
+
+    /// <summary>
+    /// 재장전 조작 함수
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetReloadTime(int value)
+    {
+        baseWeapon.reloadTime = value;
+    }
+    
+    /// <summary>
+    /// 재장전 반환 함수
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public float GetReloadTime()
+    {
+        return baseWeapon.reloadTime;
+    }
+    
+    // TODO: 공속 추가 필요
+    // /// <summary>
+    // /// 과 조작 함수
+    // /// </summary>
+    // /// <param name="value"></param>
+    // public void SetReloadTime(int value)
+    // {
+    //     baseWeapon.reloadTime = value;
+    // }
+    //
+    // /// <summary>
+    // /// 재장전 반환 함수
+    // /// </summary>
+    // /// <param name="value"></param>
+    // /// <returns></returns>
+    // public float GetReloadTime()
+    // {
+    //     return baseWeapon.reloadTime;
+    // }
 }
