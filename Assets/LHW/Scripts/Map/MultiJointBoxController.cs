733a009e93ae5417cfe7969b7dc9707a1a5472f7
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-07-27 13:35:50 +0900

    [Feat] MultiJoint 부술 수 있는 플랫폼 네트워크 동기화
    플랫폼 중 두 개 이상의 오브젝트와 연결된 오브젝트 네트워크 동기화 및 테스트

--- Assets/LHW/Scripts/Map/MultiJointBoxController.cs
+++ Assets/LHW/Scripts/Map/MultiJointBoxController.cs
@@ -1,11 +1,18 @@
+using Photon.Pun;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class MultiJointBoxController : MonoBehaviour
+public class MultiJointBoxController : MonoBehaviourPun, IPunObservable
 {
     FixedJoint2D[] fixedJoint;
 
     private Rigidbody2D rigid;
+
+    // 네트워크 동기화용 변수
+    private bool isPhysicsEnabled = false;
+    private bool networkPhysicsEnabled = false;
+    private Vector3 networkPos;
+    private Quaternion networkRot;
 
     private void Awake()
     {
@@ -17,32 +24,87 @@ public class MultiJointBoxController : MonoBehaviour
     {
         if (FixedJointDisabled())
         {
-            rigid.bodyType = RigidbodyType2D.Dynamic;
-            rigid.mass = 0.1f;
-            rigid.gravityScale = 0.3f;
+            EnablePhysics();
+        }
+        if (!photonView.IsMine)
+        {
+            if (isPhysicsEnabled)
+            {
+                transform.position = Vector3.Lerp(transform.position, networkPos, Time.deltaTime * 10f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, networkRot, Time.deltaTime * 10f);
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Bullet") || collision.gameObject.CompareTag("Hammer"))
+        if (photonView.IsMine && (collision.gameObject.CompareTag("Bullet") || collision.gameObject.CompareTag("Hammer")))
         {
-            rigid.bodyType = RigidbodyType2D.Dynamic;
-            rigid.mass = 0.1f;
-            rigid.gravityScale = 0.3f;
-        }
+            EnablePhysics();
+        }        
     }
 
     private bool FixedJointDisabled()
     {
         for (int i = 0; i < fixedJoint.Length; i++)
         {
-            if (fixedJoint != null && fixedJoint[i].IsDestroyed() || (fixedJoint != null && fixedJoint[i].connectedBody != null && fixedJoint[i].connectedBody.bodyType == RigidbodyType2D.Dynamic))
+            if (photonView.IsMine && (fixedJoint[i].IsDestroyed()))
+                
             {
-
                 return true;
             }
         }
         return false;
+    }
+
+    private void EnablePhysics()
+    {
+        networkPhysicsEnabled = true;
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            rigid.bodyType = RigidbodyType2D.Dynamic;
+            rigid.mass = 0.1f;
+            rigid.gravityScale = 0.3f;
+            isPhysicsEnabled = true;
+        }
+    }
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(isPhysicsEnabled);
+            if (isPhysicsEnabled)
+            {
+                stream.SendNext(rigid.velocity);
+                stream.SendNext(rigid.angularVelocity);
+            }
+        }
+        else if (stream.IsReading)
+        {
+            networkPos = (Vector3)stream.ReceiveNext();
+            networkRot = (Quaternion)stream.ReceiveNext();
+            networkPhysicsEnabled = (bool)stream.ReceiveNext();
+            if (networkPhysicsEnabled != isPhysicsEnabled)
+            {
+                isPhysicsEnabled = networkPhysicsEnabled;
+            }
+
+            if (isPhysicsEnabled)
+            {
+                Vector2 networkVelocity = (Vector2)stream.ReceiveNext();
+                float networkAngularVelocity = (float)stream.ReceiveNext();
+
+                if (!photonView.IsMine)
+                {
+                    rigid.velocity = networkVelocity;
+                    rigid.angularVelocity = networkAngularVelocity;
+                }
+            }
+        }
     }
 }
