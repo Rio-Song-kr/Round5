6eefe1ae44017ab8b18256c596292eebb4d1992c
GBron <zmflatod@naver.com>
2025-07-28 12:12:25 +0900

    [Feat] Laser 그을림 이펙트 적용

--- Assets/KDJ/Scripts/Laser.cs
+++ Assets/KDJ/Scripts/Laser.cs
@@ -6,6 +6,7 @@ using UnityEngine.VFX;
 
 public class Laser : MonoBehaviour
 {
+    [SerializeField] private GameObject _laserSoot; // 레이저 그을림 효과
     private VisualEffect _laserEffect;
     private RaycastHit2D[] _hits = new RaycastHit2D[10];
     private Coroutine _laserCoroutine;
@@ -28,6 +29,8 @@ public class Laser : MonoBehaviour
                 _laserCoroutine = StartCoroutine(LaserCoroutine());
             }
         }
+
+        TestLookAtMouse();
     }
 
     private void LaserBeam()
@@ -37,10 +40,12 @@ public class Laser : MonoBehaviour
         if (Physics2D.RaycastNonAlloc(transform.position, transform.right, _hits, 100f) > 0)
         {
             _laserEffect.SetVector3("EndPos", _hits[0].point); // 레이저가 충돌한 위치로 끝 위치 설정
+            _laserEffect.SetVector3("HitPos", _hits[0].point); // 파편 이펙트용 충돌 위치 설정
         }
         else
         {
             _laserEffect.SetVector3("EndPos", transform.position + transform.right * 100); // 충돌이 없으면 기본 끝 위치 설정
+            _laserEffect.SetVector3("HitPos", transform.position + transform.right * 100); // 파편 이펙트용 기본 위치 설정
         }
     }
 
@@ -49,15 +54,27 @@ public class Laser : MonoBehaviour
         _laserEffect.enabled = true;
         _laserEffect.SetFloat("Duration", Duration); // 레이저 지속 시간 설정
         float Timer = 0f;
+        GameObject soot = Instantiate(_laserSoot, transform.position, Quaternion.identity); // 레이저 그을림 효과 생성
 
         while (Timer <= Duration)
         {
             Timer += Time.deltaTime;
             LaserBeam();
+            soot.transform.position = _hits[0].point; // 그을림 효과 위치 업데이트
             yield return null; // 다음 프레임까지 대기
         }
 
         _laserEffect.enabled = false; // 지속 시간이 끝나면 레이저를 숨깁니다.
         _laserCoroutine = null; // 코루틴 종료
+    }
+
+    /// <summary>
+    /// 테스트 코드입니다.
+    /// </summary>
+    private void TestLookAtMouse()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0; // 2D 게임이므로 z축은 0으로 설정
+        transform.right = (mousePos - transform.position).normalized; // 레이저 방향을 마우스 위치로 설정
     }
 }
