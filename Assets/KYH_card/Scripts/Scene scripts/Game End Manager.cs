4ad3a955c0bfd0231e6408a12a6b8321f33ff12b
YongHo-FOG <dydgh9832@gmail.com>
2025-07-29 10:28:03 +0900

    [Feat] 게임씬에서 임시로 결과를 내어 다시 카드씬으로 회귀

--- Assets/KYH_card/Scripts/Scene scripts/Game End Manager.cs
+++ Assets/KYH_card/Scripts/Scene scripts/Game End Manager.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
 public class GameEndManager : MonoBehaviourPunCallbacks
 {
-    public void Update()
+    void Awake()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            // SceneLoadingManager.Instance.LoadSceneAsync("CardTest");
-            // SceneLoadingManager.Instance.AllowSceneActivation();
-
-            PhotonNetwork.LoadLevel("Game Scene");
+            // 마스터가 true, 다른 애는 false
+            photonView.RPC(nameof(SetIsWinner), RpcTarget.All, PhotonNetwork.IsMasterClient);
+            if (PhotonNetwork.IsMasterClient)
+            {
+                PhotonNetwork.LoadLevel("CardTest");
+            }
         }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            // 마스터가 false, 다른 애는 true
+            photonView.RPC(nameof(SetIsWinner), RpcTarget.All, !PhotonNetwork.IsMasterClient);
+            if (PhotonNetwork.IsMasterClient)
+            {
+                PhotonNetwork.LoadLevel("CardTest");
+            }
+        }
+    }
+
+    [PunRPC]
+    public void SetIsWinner(bool masterIsWinner)
+    {
+        bool isWinner = PhotonNetwork.IsMasterClient ? masterIsWinner : !masterIsWinner;
+
+        ExitGames.Client.Photon.Hashtable winnerProp = new ExitGames.Client.Photon.Hashtable
+        {
+            { "isWinner", isWinner }
+        };
+
+        PhotonNetwork.LocalPlayer.SetCustomProperties(winnerProp);
+        Debug.Log($"[GameEndManager] 나는 {(isWinner ? "승자" : "패자")}입니다.");
     }
 }
