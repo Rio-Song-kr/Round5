47c5eadb3448469f4a907f9199bd09ccd0ad3ddf
Huge-Chamchi <abaebae96@gmail.com>
2025-07-31 02:57:05 +0900

    [Feat] 인게임씬 연동 구현
    [Feat] 인게임씬 연동 구현

---
+++ Assets/USW/GameScene/TempPlayManager.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+public class TempPlayManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject _PlayerPrefab;
+    [SerializeField] private Camera _camera;
+    
+    [Header("Player Spawn Positions")]
+    [SerializeField] private Transform[] playerSpawnPoints = new Transform[2];
+    [SerializeField] private Vector2[] defaultSpawnPositions = new Vector2[2] 
+    { 
+        new Vector2(-5f, -0.7f),  
+        new Vector2(5f, -0.7f)   
+    };
+
+    private bool hasSpawned = false;
+
+    private void Start()
+    {
+        SpawnPlayer();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        // 중복 생성 방지
+        if (hasSpawned) return;
+        
+        SpawnPlayer();
+    }
+    
+    /// <summary>
+    /// 플레이어를 고정된 위치에 생성
+    /// </summary>
+    private void SpawnPlayer()
+    {
+        Vector2 spawnPosition = GetPlayerSpawnPosition();
+        
+        GameObject player = PhotonNetwork.Instantiate("TempPlayer", spawnPosition, Quaternion.identity);
+        hasSpawned = true;
+    }
+    
+    /// <summary>
+    /// 플레이어 순서에 따른 스폰 위치 결정
+    /// </summary>
+    private Vector2 GetPlayerSpawnPosition()
+    {
+        int playerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
+        
+        // Transform이 설정되어 있으면 Transform 사용
+        // 각 씬마다 배치하기 위함.
+        if (playerSpawnPoints != null && playerSpawnPoints.Length > playerIndex && 
+            playerSpawnPoints[playerIndex] != null)
+        {
+            return playerSpawnPoints[playerIndex].position;
+        }
+        
+        // Transform이 없으면 기본 위치 사용
+        // 이건 임시용
+        if (playerIndex < defaultSpawnPositions.Length)
+        {
+            return defaultSpawnPositions[playerIndex];
+        }
+        
+        // 예외 상황 - 기본값 반환
+        return Vector2.zero;
+    }
+    
+    /// <summary>
+    /// 에디터에서 스폰 포인트 시각화
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        // Transform 스폰 포인트 그리기
+        if (playerSpawnPoints != null)
+        {
+            for (int i = 0; i < playerSpawnPoints.Length; i++)
+            {
+                if (playerSpawnPoints[i] != null)
+                {
+                    Gizmos.color = i == 0 ? Color.blue : Color.red;
+                    Gizmos.DrawWireSphere(playerSpawnPoints[i].position, 0.5f);
+                    Gizmos.DrawWireCube(playerSpawnPoints[i].position, Vector3.one);
+                }
+            }
+        }
+        
+        // 기본 스폰 위치 그리기
+        for (int i = 0; i < defaultSpawnPositions.Length; i++)
+        {
+            Gizmos.color = i == 0 ? Color.cyan : Color.yellow;
+            Gizmos.DrawWireSphere(defaultSpawnPositions[i], 0.3f);
+        }
+    }
+}
---
+++ Assets/USW/LobbyScene/Script/LoadingSceneManager.cs
@@ -0,0 +1,190 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+using DG.Tweening;
+
+public class LoadingSceneManager : MonoBehaviourPunCallbacks
+{
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI player1NameText;
+    [SerializeField] private TextMeshProUGUI player2NameText;
+    
+    [Header("Animation Settings")]
+    [SerializeField] private float nameAnimationDuration = 2f;
+    [SerializeField] private float delayBetweenAnimations = 0.5f;
+    [SerializeField] private float loadingDelay = 3f; 
+    
+    [Header("Name Animation Positions")]
+    [SerializeField] private Transform player1StartPos;
+    [SerializeField] private Transform player1EndPos;
+    [SerializeField] private Transform player2StartPos;
+    [SerializeField] private Transform player2EndPos;
+    
+    private string gameSceneName = "DemoScene"; 
+    private bool isLoadingComplete = false;
+    
+    private void Start()
+    {
+        InitializeLoadingScene();
+    }
+    
+    private void InitializeLoadingScene()
+    {
+        // 플레이어 닉네임 설정
+        SetupPlayerNames();
+        
+        // 로딩 애니메이션 시작
+        StartCoroutine(LoadingSequence());
+    }
+    
+    /// <summary>
+    /// 플레이어 닉네임 설정
+    /// </summary>
+    private void SetupPlayerNames()
+    {
+        Player[] players = PhotonNetwork.PlayerList;
+        
+        if (players.Length >= 2)
+        {
+            string player1Name = players[0].NickName;
+            string player2Name = players[1].NickName;
+            
+            // 자신이 플레이어 1인지 2인지에 따라 위치 결정
+            if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
+            {
+                if (player1NameText) player1NameText.text = player1Name;
+                if (player2NameText) player2NameText.text = player2Name;
+            }
+            else
+            {
+                if (player1NameText) player1NameText.text = player2Name;
+                if (player2NameText) player2NameText.text = player1Name;
+            }
+        }
+        else
+        {
+            // 디버그 때는 임시로
+            if (player1NameText) player1NameText.text = "Player1";
+            if (player2NameText) player2NameText.text = "Player2";
+        }
+    }
+    
+    /// <summary>
+    /// 로딩 시퀀스
+    /// </summary>
+    private IEnumerator LoadingSequence()
+    {
+        // 1단계: 닉네임 교차 애니메이션
+        yield return StartCoroutine(PlayNameCrossAnimation());
+        
+        // 2단계: 게임 씬 로딩
+        yield return StartCoroutine(LoadGameScene());
+    }
+    
+    /// <summary>
+    /// 닉네임 교차 애니메이션 (DOTween pro로 대체할 예정임)
+    /// </summary>
+    private IEnumerator PlayNameCrossAnimation()
+    {
+        
+        yield return StartCoroutine(SimpleNameAnimation());
+        
+        yield return new WaitForSeconds(nameAnimationDuration);
+    }
+    
+    /// <summary>
+    /// 간단한 이름 애니메이션 
+    /// </summary>
+    private IEnumerator SimpleNameAnimation()
+    {
+        float timer = 0f;
+        
+        while (timer < nameAnimationDuration)
+        {
+            timer += Time.deltaTime;
+            float progress = timer / nameAnimationDuration;
+            
+            // 알파값 애니메이션
+            if (player1NameText)
+            {
+                Color color = player1NameText.color;
+                color.a = Mathf.Lerp(0f, 1f, progress);
+                player1NameText.color = color;
+            }
+            
+            if (player2NameText)
+            {
+                Color color = player2NameText.color;
+                color.a = Mathf.Lerp(0f, 1f, progress * 0.8f + 0.2f);
+                player2NameText.color = color;
+            }
+            
+            yield return null;
+        }
+    }
+    
+    /// <summary>
+    /// 게임 씬 로딩
+    /// </summary>
+    private IEnumerator LoadGameScene()
+    {
+        // 로딩 대기 시간
+        yield return new WaitForSeconds(loadingDelay);
+        
+        // 마스터 클라이언트만 씬 로드
+        if (PhotonNetwork.IsMasterClient && !isLoadingComplete)
+        {
+            isLoadingComplete = true;
+            PhotonNetwork.LoadLevel(gameSceneName);
+        }
+    }
+    
+    /// <summary>
+    /// 에디터에서 위치 시각화
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        
+        if (player1StartPos != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(player1StartPos.position, 0.5f);
+        }
+        
+        if (player2StartPos != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(player2StartPos.position, 0.5f);
+        }
+        
+        
+        if (player1EndPos != null)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(player1EndPos.position, 0.5f);
+        }
+        
+        if (player2EndPos != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(player2EndPos.position, 0.5f);
+        }
+    }
+    
+    #region Photon Callbacks
+    
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel("LobbyScene");
+        }
+    }
+    
+    #endregion
+}
