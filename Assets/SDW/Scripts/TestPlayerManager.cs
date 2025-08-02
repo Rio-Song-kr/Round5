35fa4dbb77a5a0c829b37c86e0d08e829d9e7c89
Rio_Song <rio.dw.song@gmail.com>
2025-08-03 00:41:53 +0900

    [Feat] Abyssal Countdown 세부 effect 구현 완료 및 Laser의 Network 로직 수정
    * Abyssal Countdown 세부 effect 구현 완료
      * Abyssal Countdown 활성화 시 영역 내 다른 플레이어가 들어올 경우, 끌어당김 효과
      * 플레이어 이동 관련 테스트 코드 추가 후 이동 중/이동 중이 아닐 때 무적 쉴드 지속 시간 관련 로직 수정
    * Laser의 Network 로직 수정

--- Assets/SDW/Scripts/TestPlayerManager.cs
+++ Assets/SDW/Scripts/TestPlayerManager.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 
 public class TestPlayerManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private GameObject _PlayerPrefab;
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private GameObject _laserPrefab;
     [SerializeField] private Camera _camera;
     [SerializeField] private PoolManager _pools;
 
     private void Awake()
     {
         _pools = FindFirstObjectByType<PoolManager>();
-        _pools.InitializePool(_PlayerPrefab.name, _PlayerPrefab, 1, 2);
+        _pools.InitializePool(_playerPrefab.name, _playerPrefab, 1, 2);
+        _pools.InitializePool(_laserPrefab.name, _laserPrefab, 1, 2);
     }
 
     public override void OnJoinedRoom()
     {
         Debug.Log("방에 참가하셨습니다.");
-        PhotonNetwork.Instantiate(_PlayerPrefab.name, new Vector2(Random.Range(-8f, 0), Random.Range(-4f, 4f)),
+        PhotonNetwork.Instantiate(_playerPrefab.name, new Vector2(Random.Range(-8f, 0), Random.Range(-4f, 4f)),
             Quaternion.identity);
+
+        PhotonNetwork.Instantiate(_laserPrefab.name, Vector3.zero, Quaternion.Euler(0, 90, 0));
     }
 }
