8c2a7713ff82b548279738ae15adfbe761566be1
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-08-08 00:24:07 +0900

    [Feat] 오류 수정 & 카드뽑기 캐릭터 애니메이션 수정
    StaticScoreUI IndexOut 오류 생기지 않도록 제한사항 추가, 카드 뽑기 애니메이션 몸통 위치 초기화 및 손 타겟 위치 수정
    * 인게임 반영 안되어 있으며 프리팹으로 빼둠

--- Assets/KYH_card/Scripts/CharacterShrinkEffect.cs
+++ Assets/KYH_card/Scripts/CharacterShrinkEffect.cs
@@ -8,6 +8,9 @@ public class CharacterShrinkEffect : MonoBehaviourPun
 {
     [SerializeField] private Transform characterTransform;
 
+    [SerializeField] private GameObject body;
+    [SerializeField] private Transform bodyDefaultTransform;
+
     private Vector3 originalScale;
 
     [Header("애니메이션 커브")]
@@ -25,10 +28,17 @@ public class CharacterShrinkEffect : MonoBehaviourPun
         originalScale = characterTransform.localScale;
     }
 
+    private void OnEnable()
+    {
+        gameObject.transform.localScale = originalScale;
+        body.transform.position = bodyDefaultTransform.position;
+    }
+
     [PunRPC]
     public void RPC_PlayShrinkAnimation()
     {
-        Vector3 shrinkScale = Vector3.one * 0.2f;
+        // 이형원 수정 - Vector3.one * 0.2f 로 할 이유가 있나...?
+        Vector3 shrinkScale = Vector3.zero;
 
         Sequence seq = DOTween.Sequence();
 
@@ -36,7 +46,7 @@ public class CharacterShrinkEffect : MonoBehaviourPun
         seq.Append(characterTransform.DOScale(shrinkScale, 1f).SetEase(shrinkCurve));
 
         // 커브GrowFast로 0.2초간 복원
-        seq.Append(characterTransform.DOScale(originalScale, 0.4f).SetEase(growCurve));
+        //seq.Append(characterTransform.DOScale(originalScale, 0.4f).SetEase(growCurve));
     }
 
     public void RequestShrinkEffect()
