a8bf4bd691f1b0444f0571343fb0a024685d5a61
YongHo-FOG <dydgh9832@gmail.com>
2025-08-09 15:20:09 +0900

    [Feat] 새로운 팝사운드 등록 및 사운드 출력상황 조정

--- Assets/KYH_card/SplashManager.ver2.cs
+++ Assets/KYH_card/SplashManager.ver2.cs
@@ -20,7 +20,7 @@ public class SplashManagerver2 : MonoBehaviour
     {
         StartCoroutine(PlaySplashSequence());
 
-        
+
     }
     private void Update()
     {
@@ -40,30 +40,38 @@ public class SplashManagerver2 : MonoBehaviour
         logoImage.color = new Color(1, 1, 1, 0);
         logo.SetActive(true);
 
+        yield return new WaitForSeconds(2f);
+
         Sequence seq = DOTween.Sequence();
         seq.Append(logoImage.DOFade(1, fadeInTime));
         seq.AppendInterval(stayTime);
         seq.Append(logoImage.DOFade(0, fadeOutTime));
 
-        // 첫 시퀀스 끝난 뒤 두 번째 시작
         seq.OnComplete(() =>
         {
-            logo.SetActive(false); // 첫 로고 비활성화
-            AcademiLogo.color = new Color(1, 1, 1, 0);
-            Academi.SetActive(true);
+            StartCoroutine(NextSequence());
+        });
+    }
 
-            SoundManager.Instance.PlaySFX("Drum");
+    IEnumerator NextSequence()
+    {
+        SoundManager.Instance.PlaySFX("Pop");
+        logo.SetActive(false);
+        AcademiLogo.color = new Color(1, 1, 1, 0);
+        Academi.SetActive(true);
 
-            SoundManager.Instance.PlaySFX("KYUNGIL");
-            Sequence seq2 = DOTween.Sequence();
-            seq2.Append(AcademiLogo.DOFade(1, fadeInTime));
-            seq2.AppendInterval(stayTime);
-            seq2.Append(AcademiLogo.DOFade(0, fadeOutTime));
-            seq2.OnComplete(() =>
-            {
-                SceneManager.LoadScene(nextSceneName);
-                SoundManager.Instance.PlayBGMLoop("LoginBGM");
-            });
+        yield return new WaitForSeconds(1f);
+
+        
+        Sequence seq2 = DOTween.Sequence();
+        seq2.Append(AcademiLogo.DOFade(1, fadeInTime));
+        seq2.AppendInterval(stayTime);
+        SoundManager.Instance.PlaySFX("KYUNGIL");
+        seq2.Append(AcademiLogo.DOFade(0, fadeOutTime));
+        seq2.OnComplete(() =>
+        {
+            SceneManager.LoadScene(nextSceneName);
+            SoundManager.Instance.PlayBGMLoop("LoginBGM");
         });
     }
 }
